e9589d743a4f7d8c3fb06cfbad1ae4c70d79813d
Sariah <sariahmghames@gmail.com>
2024-04-04 14:34:50 +0100

    adding repo for launching the motion prediction nodes

--- motion_predict/neurosym_sgan/setup.py
+++ motion_predict/neurosym_sgan/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=['neurosym_sgan'],
-    package_dir={'neurosym_sgan': 'src/neuROSym/motion_predict'}
+    package_dir={'neurosym_sgan': 'src/neuROSym/motion_predict/neurosym_sgan'}
 )
 
 setup(**setup_args)
